79dbeb069808812f1dc22fe56410f7b8f736c89b
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2026-09-26 12:41:40 +0330

    test: isolate MSSQL factory coverage
    Signed-off-by: Albert Heidarian <50908145+baapar1101@users.noreply.github.com>

--- packages/nocodb/src/db/sql-client/lib/SqlClientFactoryIntegration.spec.ts
+++ packages/nocodb/src/db/sql-client/lib/SqlClientFactoryIntegration.spec.ts
@@ -1,3 +1,10 @@
+jest.mock('./mysql/MysqlClient', () => ({ __esModule: true, default: class MysqlClient {} }));
+jest.mock('./mysql/TidbClient', () => ({ __esModule: true, default: class TidbClient {} }));
+jest.mock('./mysql/VitessClient', () => ({ __esModule: true, default: class VitessClient {} }));
+jest.mock('./pg/PgClient', () => ({ __esModule: true, default: class PgClient {} }));
+jest.mock('./pg/YugabyteClient', () => ({ __esModule: true, default: class YugabyteClient {} }));
+jest.mock('./sqlite/SqliteClient', () => ({ __esModule: true, default: class SqliteClient {} }));
+
 jest.mock('~/helpers/resolveSslFileConfig', () => ({
   resolveSslFileConfig: jest.fn(async () => undefined),
 }));
